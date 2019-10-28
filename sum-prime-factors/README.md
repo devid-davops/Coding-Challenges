f089dbfa1e557c5fe6281c5b5df0b3d541d3543c
devid-davops <55767894+devid-davops@users.noreply.github.com>
2019-10-28 17:07:43 -0400

    Create folder for sum-prime-factors

---
+++ sum-prime-factors/README.md
@@ -0,0 +1,32 @@
+# Sum of Prime Factors
+
+## Problem Statement
+Display the sum of all prime factors (if any exist) of a given number, N.
+
+>input: 30
+
+>output: 2 + 3 + 5 = 10
+
+
+## Approach
+### Assumptions
+1. 
+1. 
+
+
+### Solution Outline
+1. 
+2. 
+3. 
+
+
+## Extra Notes
+### Improvements (optional but possibly fun)
+1. 
+1. 
+
+
+### References:
+1. [Python 3 Documentation](https://docs.python.org/3/index.html)
+1. [Codecademy: Sum of Prime Factors Challenge](https://discuss.codecademy.com/t/challenge-sum-of-prime-factors/81035)
+1. [Khan Academy: Prime factorization](https://www.khanacademy.org/math/pre-algebra/pre-algebra-factors-multiples/pre-algebra-prime-factorization-prealg/v/prime-factorization)
---
+++ sum-prime-factors/code/sum-prime-factors.py
@@ -0,0 +1,66 @@
+## code block
+
+
+## code block
+
+
+
+
+## DBE's Notes
+""" Notes about Challenge:
+    1. Display the sum of all prime factors (if any exist) of a given number, N.
+        -> input: 30
+        -> output: 2 + 3 + 5 = 10
+
+    References:
+        [1] Python 3 Documentation 
+            >> https://docs.python.org/3/index.html
+        [2] Codecademy: Sum of Prime Factors Challenge
+            >> https://discuss.codecademy.com/t/challenge-sum-of-prime-factors/81035
+        [3] Khan Academy: Prime factorization
+            >> https://www.khanacademy.org/math/pre-algebra/pre-algebra-factors-multiples/pre-algebra-prime-factorization-prealg/v/prime-factorization
+"""
+
+
+# David Booker-Earley's approach for this challenge:
+    ## ** OVERVIEW **
+        # 1. Accept a valid input.
+        # 2. 
+        # 3. 
+        # 4. 
+    
+    
+    ## ** ASSUMPTIONS **
+        # 1. The integer input may have distinct or repeating prime factors (if any exist).
+        # 2. The operator for (floor) integer-division "//" rounds down.
+    
+    
+    ## ** SOLUTION **
+      # Define a function that accepts a given number N.
+        # Check for invalid inputs:
+            # IF valid: proceed (e.g., check if type is integer).
+            # IF invalid: print "Invalid entry." or do nothing.
+        
+        # Divide and Conquer -> ? :
+            # Setup sub-function(s):
+                # Block A -> Main and ?:
+                    # ?
+                
+                # Block B -> ?:
+                    # ?
+      
+      # Call main function on a given input.
+      # Print results.
+    
+    
+    ## ** IMPROVEMENTS (Extra Practice for Fun) **
+        # 1. 
+        # 2. 
+        # 3. 
+    
+    
+    ## Notes to self for Testing and Troubleshooting:
+        # Take time to conceptually go through each block of code.
+        # Track how the loops occur.
+        # Track inputs & outputs.
+        # Be sure the code & its action match conceptual approach.
