1db3ee752e54c482e21a318cd78e8596cb5fed9e
devid-davops <55767894+devid-davops@users.noreply.github.com>
2019-11-08 17:34:59 -0500

    Update Soln-1 for sum-prime-factors

--- sum-prime-factors/code/sum-prime-factors.py
+++ sum-prime-factors/code/sum-prime-factors.py
@@ -92,7 +92,7 @@ print_details( sum_unique_prime_factors(N) )
     ## ** OVERVIEW **
         # 1. Define a function that accepts a valid integer input, N.
         # 2. Set a new range of integers to consider as Prime Factors (PFs).
-        # 3. Conditionaly perform checks.
+        # 3. Conditionally perform checks.
             # 3.a. Check for any factors (unique or not) of N.
             # 3.b. Perform a "prime-check" to filter out non-prime numbers.
         # 4. Return the sum of the found (unique or not) prime factors.
@@ -118,7 +118,7 @@ print_details( sum_unique_prime_factors(N) )
                 # pf_max <= N//2
                 # -> (variable) the largest possible prime factor
             
-            # Initialize auxiliary list to store Prime Factors
+            # Initialize auxiliary list to store Prime Factors.
                 # pf = list()
             
             # For each current_integer in new A-range [2, pf_max],
@@ -127,26 +127,28 @@ print_details( sum_unique_prime_factors(N) )
                 ## the frequency of occurrence of each base factor
                 ## (e.g., 36 = 2^2 * 3^2).
                 
-                # Perform factor-check
+                # Perform factor-check.
                 # Check if N is a multiple of current_int via trial division:
                     # If N % current_int == 0: 
-                        # factor found, proceed to prime-check
-                    # Else: go to next current_int
+                        # Factor found, proceed to prime-check.
+                    # Else: go to next current_int.
                 
-                # Perform prime-check, filter out non-prime numbers
+                # Perform prime-check, filter out non-prime numbers.
                 # If factor found in A-range, Check if current_int has PF:
                     ## For each possible_factor in B-range [2, current_int):
                         # If current_int % possible_factor == 0:
-                            # Break from current loop iteration
+                            # Break from current loop iteration.
                     
                     ## Note: The else clause of a for-loop will run when no
                     ## "break" occurs, which means the current integer is 
                     ## both a factor and prime number (i.e., valid PF) of N.
                     
                     ## Else:
-                        # Append current_int to list of PF
+                        # Append current_int to list of PF.
             
-            # return tuple of details: (sum, [list], N)
+            # Conditionally return statement or results:
+                # If PF list is empty: return a string like "No PF found".
+                # Else: return a tuple of details: (sum, [list], N)
 
 
 
@@ -186,9 +188,13 @@ print_details( sum_unique_prime_factors(N) )
             # may have their own prime factors, but
             # only exist within the inclusive range: [2, N//2]
         # ---------------------------------------------------------------------------------------------- include updated example
-      
-      
-      
+        # Consider the following two integers:
+        # N = 22
+            # 
+        # N = 500
+            # 
+    
+    
     ## ** IMPROVEMENTS (Extra Practice for Fun) **
         # 1. Output the prime factors as _____ multiples of a unique base factor: 36 = 2^2 * 3^2 ... sum_unique = 2 + 3 = 5 ... sum_all = 4 + 9 = 13 ------------------------------------------ update
         # 2. Solve the original task with zero (or minimal) Trial Division.
@@ -207,101 +213,10 @@ print_details( sum_unique_prime_factors(N) )
 ## Temporary Conceptual Analysis
 """ 
 Extra Info to include, Example: ---------------------------------------------------------------------------------------------- update & include brief example
+    
     N = 22 = 2 * 11
     .. new range: [2 3 4 5 ... 10 11]
     .. [inclusive, exclusive)
-
-
-
-
-
-
-_________________________________________ Exclude info below
-
-Test these values for N:
-    -10 --> error should occur
-    -6 --> error should occur
-    2
-    3
-    4
-    6
-    14
-    15
-    36
-    50
-    75
-    154
-    155
-    500
-
-
-_________________________________________
-
-Testing main concept in Python -->> it works:
-    >>> pf = list()
-    >>> pf
-    []
-    >>>
-    >>> 
-    >>> N = 22
-    >>> 
-    >>> for CI in range(2, N//2 + 1):
-    ...     if N % CI == 0:
-    ...         # Prime Check Starts Here
-    ...         for PosFac in range(2, CI):
-    ...             if CI % PosFac == 0:
-    ...                 break
-    ...         else:
-    ...             pf.append(CI)
-    ... 
-    >>> 
-    >>> 
-    >>> pf
-    [2, 11]
-    >>>
-    >>> 
-    >>> sum(pf)
-    13
-    >>>
-
-
-_________________________________________
-
-Testing concept of TRUE-FALSE with empty sequences in Python -->> it works:
-    >>> pf = list()
-    >>> sum(pf)
-    0   
-    >>> pf
-    []  
-    >>> if not pf:         
-    ...     print("EMPTY") 
-    ... elif pf: 
-    ...     print("FULL")
-    ... 
-    EMPTY
-    >>> pf.append(2)
-    >>> pf
-    [2]
-    >>>    
-    >>> 
-    >>> if not pf:
-    ...     print("EMPTY")
-    ... elif pf:
-    ...     print("FULL")
-    ... 
-    FULL
-    >>>
-
-
-__________________________________________________________________________
-
-.. Trial Division approach should be fine for relatively small numbers, N <= 50.
-
-50 // 2
-25
-5 * 5
-
-__________________________________________________________________________
 
 
 500 can be split like this:
@@ -313,45 +228,5 @@ __________________________________________________________________________
 250
 .. Loop A: range from 2 to 250
 .. Loop B: range from 2 to the current_integer in Loop A
-.. code would iterate ineffectively looping A & B.
-
-
-
-
-
-Example A: 15
-    PFs of 15 are 3 and 5.
-    
-    Use "//" to get the max value (upper-bound) for the range of pfs for 15:
-        15 // 2 = 7
-        Range of PFs: [2, 7]
-
-
-
-
-Example B: 75
-    Ask "Can I divide 75 by 2, how about 3, or 5, etc.?
-    75 can be split with integers (no "//") like this:
-        .. 75
-        .. 3 * 25
-        .. 3 * 5 * 5    
-
-
-
-
-Example C: 154
-        .. 2 * 77
-        .. 2 * 7 * 11
-
-Visualize with integer-division:
-.. Note that using "N // 2" will provide only the range of PFs,
-.. Using the "//" operator carelessly may result in betrayal for odd numbers.
-154 // 2 (also for "155 // 2")
-        ,,
-    2       77
-            ,,
-        7      11
-
-
 
 """
